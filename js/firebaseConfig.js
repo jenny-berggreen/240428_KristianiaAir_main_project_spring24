09caf2d53e56d4408ee392a715c05a61d1d0d00f
jenny-berggreen <jenny@berggreen.no>
2024-04-29 13:38:29 +0200

    Firebase installation and setup

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAaZFhadEqw2p-TEc-fNzDQdpnOFet_cSM",
+	authDomain: "main-project-96446.firebaseapp.com",
+	projectId: "main-project-96446",
+	storageBucket: "main-project-96446.appspot.com",
+	messagingSenderId: "985267879979",
+	appId: "1:985267879979:web:c5405bb5fed3f3e195aa6e"
+  };
+
+  export default firebaseConfig
