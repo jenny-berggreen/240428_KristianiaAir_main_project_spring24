dbf789038c037d3569c75222c212f44e5fbe9988
jenny-berggreen <jenny@berggreen.no>
2024-05-01 14:41:19 +0200

    Create array of destination objects

---
+++ js/destinations.js
@@ -0,0 +1,49 @@
+// DESTINATIONS ARRAY
+const destinations = [
+	{
+		name: 'Alicante, Spain',
+		price: 'kr 699',
+		image: '/assets/images/alicante.jpeg' // https://www.apollo.no/spania/alicante
+	},
+	{
+		name: 'Amsterdam, Nederland',
+		price: 'kr 599',
+		image: '/assets/images/amsterdam.jpg' // https://storbyferie.com/amsterdam/
+	},
+	{
+		name: 'Bordeaux, Frankrike',
+		price: 'kr 799',
+		image: '/assets/images/bordeaux.jpeg' // https://snl.no/Bordeaux
+	},
+	{
+		name: 'Edinburgh, Storbritannia',
+		price: 'kr 499',
+		image: '/assets/images/edinburgh.webp' // https://www.cntraveller.com/article/travel-guide-edinburgh
+	},
+	{
+		name: 'Gran Canaria, Spania',
+		price: 'kr 949',
+		image: '/assets/images/gran-canaria.jpeg' // https://www.apollo.no/spania/kanarioyene/gran-canaria
+	},
+	{
+		name: 'Napoli, Italia',
+		price: 'kr 649',
+		image: '/assets/images/napoli.jpeg' // https://no.tripadvisor.com/Attractions-g187785-Activities-Naples_Province_of_Naples_Campania.html
+	},
+	{
+		name: 'Nice, Frankrike',
+		price: 'kr 499',
+		image: '/assets/images/nice.jpg' // https://storbyferie.com/nice/
+	},
+	{
+		name: 'Riga, Latvia',
+		price: 'kr 349',
+		image: '/assets/images/riga.jpeg' // https://storbyferie.com/riga/
+	},
+	{
+		name: 'Santorini, Hellas',
+		price: 'kr 1029',
+		image: '/assets/images/santorini.webp' // https://no.hotels.com/go/greece/things-to-do-santorini
+	}
+
+]
