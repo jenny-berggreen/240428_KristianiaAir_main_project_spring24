37b5e2db30dcfdf0f61aef18ca08539264a0b422
jenny-berggreen <jenny@berggreen.no>
2024-05-02 11:58:51 +0200

    Hide dropdown at click outside of input

--- js/fetch-countries-cities.js
+++ js/fetch-countries-cities.js
@@ -22,38 +22,41 @@ const displayFilteredCountries = (countries) => {
     searchInput.addEventListener('input', function() {
         const searchTerm = this.value.trim().toLowerCase();
         
-        // Filter countries based on the search term
+        // filter countries based on the search term
         const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(searchTerm));
         
-		// Sort filtered countries alphabetically
+		// sort filtered countries alphabetically
 		filteredCountries.sort((a, b) => {
 			const nameA = a.name.common.toLowerCase();
 			const nameB = b.name.common.toLowerCase();
 			return nameA.localeCompare(nameB);
 		});
         
-        // Clear previous results
+        // clear previous results
         dropdown.innerHTML = '';
 
-        // Create dropdown items for each filtered country
+        // create dropdown items for each filtered country
         filteredCountries.forEach(country => {
             const dropdownItem = document.createElement('div');
             dropdownItem.classList.add('dropdown-item');
             dropdownItem.textContent = country.name.common;
             
-            // Add click event listener to each dropdown item
+			// select country
             dropdownItem.addEventListener('click', function() {
-                // Set the search input value to the selected country
                 searchInput.value = country.name.common;
-                // Clear the dropdown
                 dropdown.innerHTML = '';
             });
 
-            // Append dropdown item to the dropdown list
+            // append dropdown item to the dropdown list
             dropdown.appendChild(dropdownItem);
         });
 
-        // Display dropdown
+        // display dropdown
         dropdown.style.display = filteredCountries.length > 0 ? 'block' : 'none';
     });
+
+	// hide dropdown at click outside of input
+	searchInput.addEventListener('blur', function() {
+		dropdown.style.display = 'none';
+	})
 };
