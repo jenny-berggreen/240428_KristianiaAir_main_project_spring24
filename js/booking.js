18b2367a7a556401432dc344adcf07772a492bd8
jenny-berggreen <jenny@berggreen.no>
2024-05-02 13:33:54 +0200

    Add update total function (not complete)

--- js/booking.js
+++ js/booking.js
@@ -4,7 +4,6 @@ const cityInput = document.querySelector('.city-input');
 const departureInput = document.querySelector('.departure-input');
 const returnInput = document.querySelector('.return-input');
 const travelersInput = document.querySelector('.travelers-input');
-
 const dropdownItems = document.querySelectorAll('.dropdown-item');
 
 const countryDetails = document.querySelector('.details-value--country');
@@ -21,27 +20,75 @@ for(let i = 0; i <= 15; i++) {
 	travelersInput.append(option);
 }
 
+// FUNCTION TO UPDATE AND DISPLAY TOTAL VALUE
+let total = 0;
+
+
+const updateTotal = () => {
+    total = 0;
+
+    if (countryDetails.textContent !== '') {
+        total += 500;
+		console.log(total);
+		console.log(countryDetails.textContent);
+		totalDetails.textContent = `kr ${total}`;
+    }
+
+    if (cityDetails.textContent !== '') {
+        total += 100;
+    }
+
+    if (departureDetails.textContent !== '' && returnDetails.textContent !== '') {
+        const departureDate = new Date(departureInput.value);
+        const returnDate = new Date(returnInput.value);
+        const travelTime = Math.ceil((returnDate - departureDate) / (1000 * 3600 * 24)); // Calculate travel time in days and round it up
+        total += travelTime * 100;
+    }
+
+    if (travelersDetails.textContent !== '') {
+        const numTravelers = parseInt(travelersInput.value);
+        total *= numTravelers;
+    }
+
+    // Update total details
+    totalDetails.textContent = `kr ${total}`;
+};
+
+
 // DISPLAY DETAILS IN SUMMARY
+// country
 dropdown.addEventListener('click', function(event) {
     if (event.target.classList.contains('dropdown-item')) {
         const clickedCountry = event.target.textContent;
         countryDetails.textContent = clickedCountry;
+		updateTotal();
+		console.log('updated' + total);
     }
 });
 
-cityInput.addEventListener('blur', function() {
+// city
+cityInput.addEventListener('input', function() {
 	cityDetails.textContent = this.value;
+	updateTotal();
 })
 
-departureInput.addEventListener('change', function() {
+// departure
+departureInput.addEventListener('input', function() {
 	departureDetails.textContent = this.value;
+	updateTotal();
 })
 
-returnInput.addEventListener('change', function() {
+// return
+returnInput.addEventListener('input', function() {
 	returnDetails.textContent = this.value;
+	updateTotal();
 })
 
+// travelers
 travelersDetails.textContent = travelersInput.value;
-travelersInput.addEventListener('change', function() {
+travelersInput.addEventListener('input', function() {
 	travelersDetails.textContent = this.value;
+	updateTotal();
 })
+
+updateTotal();
