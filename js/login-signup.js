9d63b50b49be18539cccb107759d770b6d0add24
jenny-berggreen <jenny@berggreen.no>
2024-05-07 12:49:39 +0200

    Handle signup

--- js/login-signup.js
+++ js/login-signup.js
@@ -10,11 +10,33 @@ import {
 
 import {getFirestore, collection, addDoc, getDocs} from 'firebase/firestore';
 
+import { validateSignInForm } from "./signInValidation";
+import { validateSignUpForm } from "./signUpValidation";
+
+// INITIALIZE FIREBASE
+initializeApp(firebaseConfig);
+
+// INITIALIZE AUTH SERVICE
+const authService = getAuth();
+
 // GET ELEMENTS FROM THE DOM
-const openSignupButton = document.querySelector('.open-signup-button');
-const openLoginButton = document.querySelector('.open-login-button');
+// login form
 const loginForm = document.querySelector('.form-container--login');
+const emailLabelLogin = document.querySelector('label[for="email"]');
+const emailInputLogin = document.querySelector('.email-input--login');
+const passwordInputLogin = document.querySelector('.password-input--login');
+const passwordLabelLogin = document.querySelector('label[for="password"]');
+const openSignupButton = document.querySelector('.open-signup-button');
+const loginButton = document.querySelector('.login-button');
+
+// signup form
 const signupForm = document.querySelector('.form-container--signup');
+const emailLabelSignup = document.querySelector('label[for="email-signup"]');
+const emailInputSignup = document.querySelector('.email-input--signup');
+const passwordLabelSignup = document.querySelector('label[for="password-signup"]');
+const passwordInputSignup = document.querySelector('.password-input--signup');
+const openLoginButton = document.querySelector('.open-login-button');
+const signupButton = document.querySelector('.signup-button');
 
 // open / close forms
 openSignupButton.addEventListener('click', (e) => {
@@ -29,4 +51,33 @@ openLoginButton.addEventListener('click', (e) => {
 
 	loginForm.style.display = 'flex';
 	signupForm.style.display = 'none';
+});
+
+// HANDLE SIGN UP ACTION
+const signUpUser= () => {
+	const {signUpValidStatus} = validateSignUpForm(
+		emailInputSignup,
+		passwordInputSignup
+	);
+
+	if (!signUpValidStatus()) {
+		return;
+	} else {
+		const newUser = {
+			signUpEmail: emailInputSignup.value.trim(),
+			signUpPassword: passwordInputSignup.value.trim()
+		}
+
+		createUserWithEmailAndPassword(authService, newUser.signUpEmail, newUser.signUpPassword)
+		.then(() => {
+			signupForm.reset();
+		})
+		.then((err) => console.log(err.message));
+	};
+};
+
+signupButton.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	signUpUser();
 });
