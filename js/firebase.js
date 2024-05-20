9a3e57ece4f2ab7c2b9a81425642da465faef87e
jenny-berggreen <jenny@berggreen.no>
2024-05-20 15:55:45 +0200

    Create firebase.js that intitializes firebase and exports services

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+// firebase.js
+import firebaseConfig from './firebaseConfig.js'
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
