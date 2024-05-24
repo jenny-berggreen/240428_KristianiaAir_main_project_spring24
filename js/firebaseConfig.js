93914549c7c3401cc79bdbba39e6b5331272d1c7
jenny-berggreen <jenny@berggreen.no>
2024-05-24 13:07:12 +0200

    Hide api key

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-	apiKey: "AIzaSyAaZFhadEqw2p-TEc-fNzDQdpnOFet_cSM",
-	authDomain: "main-project-96446.firebaseapp.com",
-	projectId: "main-project-96446",
-	storageBucket: "main-project-96446.appspot.com",
-	messagingSenderId: "985267879979",
-	appId: "1:985267879979:web:c5405bb5fed3f3e195aa6e"
-  };
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "main-project-96446.firebaseapp.com",
+    projectId: "main-project-96446",
+    storageBucket: "main-project-96446.appspot.com",
+    messagingSenderId: "985267879979",
+    appId: "1:985267879979:web:c5405bb5fed3f3e195aa6e"
+};
 
-  export default firebaseConfig
+export default firebaseConfig;
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const dotenv = require('dotenv-webpack');
 
 module.exports = {
     mode: 'development',
@@ -19,6 +20,10 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
     },
-    watch: true
+    watch: true,
+    plugins: [
+        new dotenv()
+    ]
 };
+
 
