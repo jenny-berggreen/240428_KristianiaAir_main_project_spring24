1454a6ed42720176072cc2ec0057ea064a716531
jenny-berggreen <jenny@berggreen.no>
2024-05-27 21:31:36 +0200

    Disable watch

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
     },
-    watch: true,
+    watch: false,
     plugins: [
         new Dotenv()
     ]
